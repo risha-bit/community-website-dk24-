82082be1032a41761b06ee9165152c983dfef78d
risha-bit <diasminora2613@gmail.com>
2026-03-09 21:14:55 +0530

    fix : max-w-full -->> max-w-7xl

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -152,7 +152,7 @@ export default function Navbar() {
             : "bg-background/80 backdrop-blur-lg",
         )}
       >
-        <div className="container mx-auto px-4 max-w-full">
+        <div className="container mx-auto px-4 max-w-7xl">
           <div className="flex h-20 items-center justify-between">
             <Link href="/" className="flex items-center space-x-2">
               <span className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-green-400">
